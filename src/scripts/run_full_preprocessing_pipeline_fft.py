554cad45293c340056e753f83283b126a28822de
mkarapka <mikolaj.karapka@gmail.com>
2026-01-04 22:40:29 +0100

    Add full preprocessing pipeline for fft

---
+++ src/scripts/run_full_preprocessing_pipeline_fft.py
@@ -0,0 +1,12 @@
+from src.common.basic_functions import get_batch_size
+from src.common.constants import Constants as consts
+from src.pipelines.preprocessing_pipeline import PreprocessingPipeline
+
+if __name__ == "__main__":
+    batch_size = get_batch_size()
+
+    pipeline_bonafide = PreprocessingPipeline(consts.bonafide, config_lst=[None])
+    pipeline_bonafide.preprocess_dataset_fft(batch_size=batch_size)
+
+    pipeline_spoof = PreprocessingPipeline(consts.spoof, config_lst=consts.tts_and_vocoders_configs)
+    pipeline_spoof.preprocess_dataset_fft(batch_size=batch_size)

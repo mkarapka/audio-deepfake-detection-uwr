23a7976e891ddebf43570dd99bdaf4af90594ab6
mkarapka <mikolaj.karapka@gmail.com>
2026-03-05 02:03:40 +0100

    test: Created best balance pipeline test

--- tests/best_balance_pipeline_test.py
+++ tests/best_balance_pipeline_test.py
@@ -1,69 +1,92 @@
+import pandas as pd
+from sklearn.dummy import DummyClassifier
+
+from src.common.constants import BalanceType as bt
 from src.common.constants import Constants as consts
-from src.models.mlp_classifier import MLPClassifier
+from src.common.constants import ExperimentPreprocessConfig
+from src.models.base_model import BaseModel
 from src.pipelines.experiments.best_balance_pipeline import BestBalancePipeline
 
 
+class DummyModel(BaseModel):
+    def __init__(self):
+        super().__init__(class_name="DummyModel")
+        self.model = DummyClassifier(strategy="most_frequent")
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+
+SIZE = 100
+
+
+def dummy_objective(trial, model, X_train, y_train, X_dev, y_dev):
+    trial.suggest_categorical("strategy", ["most_frequent", "stratified", "uniform"])
+    model.fit(X_train, y_train)
+    preds = model.predict(X_dev)
+    accuracy = (preds == y_dev).mean()
+    return accuracy
+
+
+EXPECTED_COLUMN_NAMES = [
+    "model_name",
+    "splits_balance_configs",
+    "n_trials",
+    "parameters",
+    "metrics_names",
+    "metrics_scores",
+]
+
+
 class TestBestBalancePipeline:
-    def test_initialization(self):
-        pipeline = BestBalancePipeline(
-            clf_model=MLPClassifier,
-            RATIOS_CONFIG=consts.only_mix_equal_ratio_config,
-            objective="f1",
-            is_chunk_prediction=True,
-        )
-        assert pipeline.RATIOS_CONFIG == consts.only_mix_equal_ratio_config
-        assert pipeline.objective == "f1"
-        assert pipeline.is_chunk_prediction is True
-        print("BestBalancePipeline initialization test passed.")
+    def TestRunPipeline():
+        preprocess_config = {
+            "splits_names": ["train", "dev"],
+            "fraction": 0.05,
+            "is_audio_ids_sampling": False,
+            "balance_configs": None,
+        }
+        preprocess_config = ExperimentPreprocessConfig(**preprocess_config)
 
-    def test_sample_fraction_uq_audios_from_split(self, is_train, fraction=0.1, check_size=20):
-        pipeline = BestBalancePipeline(
-            clf_model=MLPClassifier,
-            RATIOS_CONFIG=consts.only_mix_equal_ratio_config,
-            objective="f1",
-            is_chunk_prediction=True,
-        )
-        split = pipeline.train_split if is_train else pipeline.dev_split
-        sampled_split = pipeline._sample_fraction_uq_audios_from_split(
-            split=split, frac=fraction, is_train_split=is_train
-        )
+        balance_configs_for_experiment = {
+            bt.UNDERSAMPLE: [0.5, 0.75, 1.0],
+            bt.OVERSAMPLE: [0.5, 0.75, 1.0],
+            bt.MIX: [[0.5, 1.0]],
+            bt.UNBALANCED: [None],
+        }
+        experiment_config = {
+            "clf_model": DummyModel(),
+            "objective": dummy_objective,
+            "preprocess_config": preprocess_config,
+            "balances_configs": balance_configs_for_experiment,
+        }
 
-        print(
-            f"Fraction of metadata after sampling, column 'unique_audio_id': {
-                sampled_split['unique_audio_id'].head(10)
-            }"
-        )
-        if is_train:
-            assert sampled_split["unique_audio_id"].iloc[:check_size].nunique() >= int(check_size * 0.8)
-        else:
-            assert sampled_split["unique_audio_id"].iloc[:check_size].nunique() <= int(check_size * 0.2)
+        pipeline = BestBalancePipeline(**experiment_config)
+        pipeline.run()
 
-    def test_sample_fraction_from_split_basic(self, is_train, fraction=0.1, check_size=20):
-        pipeline = BestBalancePipeline(
-            clf_model=MLPClassifier,
-            RATIOS_CONFIG=consts.only_mix_equal_ratio_config,
-            objective="f1",
-            is_chunk_prediction=True,
+        expected_results = pd.DataFrame(
+            {
+                "model_name": ["DummyModel"],
+                "splits_balance_configs": [[[bt.MIX, 0.7], [bt.UNBALANCED, 0.7]]],
+                "n_trials": [20],
+                "parameters": None,
+                "metrics_names": ["accuracy"],
+                "metrics_scores": None,
+            }
         )
-        split = pipeline.train_split if is_train else pipeline.dev_split
-        sampled_split = pipeline._sample_fraction_from_split_basic(split=split, frac=fraction)
+        read_results = pd.read_csv(consts.train_results_dir / consts.best_balance_results)
 
-        print(
-            f"Fraction of metadata after basic sampling, column 'unique_audio_id': {
-                sampled_split['unique_audio_id'].head(10)
-            }"
-        )
-        assert sampled_split["unique_audio_id"].iloc[:check_size].nunique() >= int(check_size * 0.8)
+        assert (
+            read_results.columns.tolist() == EXPECTED_COLUMN_NAMES
+        ), f"Expected columns: {EXPECTED_COLUMN_NAMES}, but got: {read_results.columns.tolist()}"
+        assert read_results.get("metrics_scores").all().dtype is float, "Expected 'metrics_scores' column in results."
+        results_to_compare = read_results.drop(columns=["metrics_scores", "parameters"])
+        assert results_to_compare.equals(
+            expected_results.drop(columns=["metrics_scores", "parameters"])
+        ), "Results do not match expected values."
 
 
-TestBestBalancePipeline().test_initialization()
-print("Train fraction tests:")
-TestBestBalancePipeline().test_sample_fraction_uq_audios_from_split(is_train=True, fraction=0.1)
-print("Dev fraction tests:")
-TestBestBalancePipeline().test_sample_fraction_uq_audios_from_split(is_train=False, fraction=0.1)
-print("--------------------------------------------------")
-print("Basic sample fraction tests:")
-print("Train fraction test:")
-TestBestBalancePipeline().test_sample_fraction_from_split_basic(fraction=0.1, is_train=True)
-print("Dev fraction test:")
-TestBestBalancePipeline().test_sample_fraction_from_split_basic(fraction=0.1, is_train=False)
+TestBestBalancePipeline.TestRunPipeline()

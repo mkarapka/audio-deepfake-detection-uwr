8dd67ae3740601c1581c09960ec1846dd640e4fd
mkarapka <mikolaj.karapka@gmail.com>
2026-03-21 01:22:03 +0100

    refactor: update best balance pipeline and logistic regression classifier

--- src/models/logistic_regression_clf.py
+++ src/models/logistic_regression_clf.py
@@ -1,6 +1,6 @@
 import joblib
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 
 from src.models.base_model import BaseModel
 
@@ -25,16 +25,27 @@ class LogisticRegressionClf(BaseModel):
         self.model = joblib.load(file_path)
 
 
-def objective_acc(trial, model: LogisticRegressionClf, X_train, y_train, X_dev, y_dev, max_iter=None):
+def objective_acc(
+    trial, model: LogisticRegressionClf, X_train, y_train, X_dev, y_dev, max_iter=None, metrics=accuracy_score
+):
     C = trial.suggest_float("C", 0.01, 10.0, log=True)
     class_weight = trial.suggest_categorical("class_weight", [None, "balanced"])
     l1_ratio = trial.suggest_float("l1_ratio", 0.0, 1.0)
+    penality = trial.suggest_categorical("penalty", [None, "elasticnet"])
     if max_iter is None:
-        max_iter = trial.suggest_int("max_iter", 100, 1000)
+        max_iter = trial.suggest_int("max_iter", 200, 500)
     rs = 42
 
-    clf = model(C=C, l1_ratio=l1_ratio, class_weight=class_weight, max_iter=max_iter, random_state=rs)
+    clf = model(
+        C=C,
+        l1_ratio=l1_ratio,
+        penalty=penality,
+        class_weight=class_weight,
+        max_iter=max_iter,
+        random_state=rs,
+    )
     clf.fit(X_train, y_train)
+
     y_pred = clf.predict(X_dev)
-    accuracy = accuracy_score(y_dev, y_pred)
-    return accuracy
+    score = metrics(y_dev, y_pred)
+    return score

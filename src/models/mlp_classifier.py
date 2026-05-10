0ca237f9fc7fda38f02377cc494ad7f4ba0297ae
mkarapka <mikolaj.karapka@gmail.com>
2026-05-10 16:00:37 +0200

    feat: Add new class - MlpClassifier
    Added MlpClassifier class in src/models/mlp_classifier.py

---
+++ src/models/mlp_classifier.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+from src.models.torch_model import TorchModel
+
+
+class MlpClassifier(TorchModel):
+    def __init__(self, input_size: int, hidden_sizes: list, dropout_rate: float, device: str = None):
+        model = self._create_model(input_size=input_size, hidden_sizes=hidden_sizes, dropout_rate=dropout_rate)
+        super().__init__(model=model, class_name=self.__class__.__name__, device=device)
+
+    def _create_model(self, input_size: int, hidden_sizes: list[int], dropout_rate: float):
+        layers = []
+        current_size = input_size
+
+        for hidden_size in hidden_sizes:
+            layers.append(nn.Linear(current_size, hidden_size))
+            layers.append(nn.BatchNorm1d(hidden_size))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(dropout_rate))
+            current_size = hidden_size
+
+        layers.append(nn.Linear(current_size, 1))
+        return nn.Sequential(*layers)
+
+    def forward(self, X: torch.Tensor):
+        y_hat = self.model(X)
+        return y_hat
